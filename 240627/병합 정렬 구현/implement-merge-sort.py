29ea1b8ac5a36943bcb6459c26861ae4ac89365b
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-06-27 11:47:44 +0900

    Title: 병합 정렬 구현, Time: 348ms, Memory: 32MB, Status: Passed - CodeTree

--- 240627/병합 정렬 구현/implement-merge-sort.py
+++ 240627/병합 정렬 구현/implement-merge-sort.py
@@ -1,16 +1,3 @@
-# def merge(arr, low, mid, high):
-#     arr_merge = []
-#     arr1 = arr[low:mid]
-#     arr2 = arr[mid:high]
-
-#     for i in range(len(arr1) + len(arr2)):
-#         if arr1[0] <= arr2[0] or len(arr2) == 0:
-#             arr_merge.append(arr1.pop(0))
-#         elif arr1[0] > arr2[0] or len(arr1) == 0:
-#             arr_merge.append(arr2.pop(0))
-
-#     return arr_merge
-
 # def merge(arr1, arr2):
 #     arr_merge = []
 
@@ -43,49 +30,46 @@
 # arr = list(map(str, arr))
 # print(" ".join(arr))
 
-# 변수 선언 및 입력
-n = int(input())
-arr = list(map(int, input().split()))
-merged_arr = [0] * n 
-
-
 def merge(low, mid, high):
-    i, j = low, mid + 1                 # 각 리스트 내의 원소 위치를 잡습니다.
-    
-    k = low                             # 병합 시 원소를 담을 위치를 유지합니다.
-    while i <= mid and j <= high:       # 두 리스트 내의 원소가 아직 남아있다면
-        if arr[i] <= arr[j]:            # 첫 번째 리스트 내의 원소가 더 작다면
-            merged_arr[k] = arr[i]      # 해당 원소를 옮겨줍니다. 
-            k += 1
+    i = low
+    j = mid + 1
+    k = low
+
+    while i <= mid and j <= high:
+        if arr[i] <= arr[j]:
+            arr_merge[k] = arr[i]
             i += 1
         else:
-            merged_arr[k] = arr[j]      # 그렇지 않다면 두 번째 리스트 내의 원소를 옮겨줍니다.
-            k += 1
+            arr_merge[k] = arr[j]
             j += 1
 
-    while i <= mid:                     # 아직 첫 번째 리스트 내 원소가 남아있다면
-        merged_arr[k] = arr[i]          # 남은 원소들을 전부 옮겨줍니다.
         k += 1
-        i += 1
-    
-    while j <= high:                    # 아직 두 번째 리스트 내 원소가 남아있다면
-        merged_arr[k] = arr[j]          # 남은 원소들을 전부 옮겨줍니다.
-        k += 1
-        j += 1
-    
-    for k in range(low, high + 1):      # 병합된 리스트를 다시
-        arr[k] = merged_arr[k]          # 원본 리스트에 옮겨줍니다.
 
+    while i <= mid:
+        arr_merge[k] = arr[i]
+        i += 1
+        k += 1
+    
+    while j <= high:
+        arr_merge[k] = arr[j]
+        j += 1
+        k += 1
+
+    for k in range(low, high+1):
+        arr[k] = arr_merge[k]
 
 def merge_sort(low, high):
-    if low < high:                    # 원소의 개수가 2개 이상인 경우에만 진행
-        mid = (low + high) // 2       # 가운데 원소의 위치
-        merge_sort(low, mid)          # 왼쪽 부분에 대해 병합정렬 진행
-        merge_sort(mid + 1, high)     # 우측 부분에 대해 병합정렬 진행
-        merge(low, mid, high)         # 정렬된 두 리스트를 하나로 병합
+    if low < high:
+        mid = (low + high) // 2
+        merge_sort(low, mid)
+        merge_sort(mid+1, high)
+        merge(low, mid, high)
 
+n = int(input())
+arr = list(map(int, input().split()))
+arr_merge = [0] * n
 
-merge_sort(0, n - 1)
+merge_sort(0, n-1)
 
-for elem in arr:
-    print(elem, end=" ")
+for i in arr:
+    print(i, end=' ')
