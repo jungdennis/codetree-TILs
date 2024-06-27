d63a8ce7b8cb347ebb204ead630deb18ec428139
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-06-27 13:03:55 +0900

    Title: 퀵 정렬 구현, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240627/퀵 정렬 구현/implement-quick-sort.py
@@ -0,0 +1,58 @@
+def select_pivot(low, high):
+    length = high - low + 1
+
+    if length <= 3:
+        return high, arr[high]
+    else:
+        mid = (low + high) // 2
+
+        low_ = arr[low]
+        mid_ = arr[mid]
+        high_ = arr[high]
+
+        med_sort = [low_, mid_, high_]
+        med_sort.sort()
+
+        median = med_sort[1]
+        
+        if low_ == median:
+            idx = low
+        elif mid_ == median:
+            idx = mid
+        elif high_ == median:
+            idx = high
+
+        return idx, median
+
+def swap(i, j):
+    if i != j:
+        arr[i], arr[j] = arr[j], arr[i]
+
+def partition(low, high):
+    idx, pivot = select_pivot(low, high)
+    swap(idx, high)
+
+    i = 0
+    for j in range(low, high):
+        if arr[j] < pivot:
+            swap(i, j)
+            i += 1
+
+    swap(i, high)
+
+    return i
+
+def quick_sort(low, high):
+    if low < high:
+        pos = partition(low, high)
+
+        quick_sort(low, pos-1)
+        quick_sort(pos+1, high)
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+quick_sort(0, n-1)
+
+for i in arr:
+    print(i, end=' ')
