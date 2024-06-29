c8d1515a25da0aeb20d134533921d72de64b96e3
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-06-29 14:26:09 +0900

    Title: 괄호 문자열의 적합성 판단, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240629/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -0,0 +1,16 @@
+order = input()
+s = []
+
+for c in order:
+    if c == '(':
+        s.append(c)
+    else:
+        if s[-1] == '(':
+            s.pop()
+        else:
+            s.append(c)
+
+if len(s) != 0:
+    print('No')
+else:
+    print('Yes')
