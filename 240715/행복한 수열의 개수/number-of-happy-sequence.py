1f2ffd0f32ef327212c290f02ef78418e396cb34
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-07-15 18:29:12 +0900

    Title: 행복한 수열의 개수, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240715/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240715/행복한 수열의 개수/number-of-happy-sequence.py
@@ -19,14 +19,12 @@ for r in range(n):
         elif grid[r][j] != grid[r][j+1]:
             cnt = 1
 
-        if cnt >= max_cnt:
-                max_cnt = cnt
+        if cnt >= m:
+            happy += 1
+            break
 
     #     print(f"{grid[r][j]} - {grid[r][j+1]} : {cnt}, {max_cnt}")
     # print(f"row {r}:{max_cnt}")
-    
-    if max_cnt >= m:
-        happy += 1
 
 for c in range(n):
     max_cnt = 1
@@ -41,13 +39,11 @@ for c in range(n):
         elif grid[i][c] != grid[i+1][c]:
             cnt = 1
 
-        if cnt >= max_cnt:
-                max_cnt = cnt
+        if cnt >= m:
+            happy += 1
+            break
                 
     #     print(f"{grid[i][c]} - {grid[i+1][c]} : {cnt}, {max_cnt}")
     # print(f"col {c}:{max_cnt}")
-    
-    if cnt >= m:
-        happy += 1
 
 print(happy)
