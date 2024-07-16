c7c2ee0bfe1ccf8ba821d98ac41a0c01709e0c8a
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-07-16 18:06:50 +0900

    Title: 양수 직사각형의 최대 크기, Time: 158ms, Memory: 26MB, Status: Passed - CodeTree

--- 240716/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240716/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -2,17 +2,14 @@ import sys
 
 def find_square(grid, h, w, r, c):
     cnt = 0
-    if (r+h-1 >= n) or (c+w-1 >= m):
-        return 0
-    else:
-        for i in range(h):
-            for j in range(w):
-                if grid[r+i][c+j] <= 0:
-                    return 0
-                else:
-                    cnt += 1
+    for i in range(h):
+        for j in range(w):
+            if grid[r+i][c+j] <= 0:
+                return -1
+            else:
+                cnt += 1
     
-        return cnt
+    return cnt
 
 
 n, m = map(int, sys.stdin.readline().rstrip().split())
@@ -20,11 +17,14 @@ grid = []
 for i in range(n):
     grid.append(list(map(int, sys.stdin.readline().rstrip().split())))
 
-max_cnt = 0
+max_cnt = -1
 for r in range(n):
     for c in range(m):
-        for h in range(1, n+1):
-            for w in range(1, m+1):
+        max_h = n - r
+        max_w = m - c
+
+        for h in range(1, max_h+1):
+            for w in range(1, max_w+1):
                 cnt = find_square(grid, h, w, r, c)
                 # if cnt != 0:
                 #     print(h, w, r, c, cnt)
