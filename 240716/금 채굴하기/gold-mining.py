0f13e1235b711fd5d7db3f1778b78e73350d4c77
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-07-16 16:52:54 +0900

    Title: 금 채굴하기, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240716/금 채굴하기/gold-mining.py
@@ -0,0 +1,42 @@
+import sys
+
+def gold_mine(grid, n, m, k):
+    max_gold = 0
+    for r in range(n):
+        for c in range(m):
+            gold = 0
+            grid_list = []
+            for j in range(-k, k+1):
+                r_range = k - abs(j)
+                for i in range(-r_range, r_range+1):
+                    if (r+i < 0) or (r+i >= n) or (c+j < 0) or (c+j >= n):
+                        pass
+                    else:
+                        gold += grid[r+i][c+j]
+                        grid_list.append((r+i, c+j))
+            if gold > max_gold:
+                max_gold = gold
+
+    return max_gold
+
+n, m = map(int, sys.stdin.readline().rstrip().split())
+
+grid = []
+
+for i in range(n):
+    grid.append(list(map(int, sys.stdin.readline().rstrip().split())))
+
+k = 0
+max_gold = 0
+
+while 1:
+    cost = (k ** 2) + ((k + 1) ** 2)
+    gold = gold_mine(grid, n, m, k)
+    if gold * m < cost:
+        break
+    else:
+        if gold > max_gold:
+            max_gold = gold
+        k += 1
+
+print(max_gold)
