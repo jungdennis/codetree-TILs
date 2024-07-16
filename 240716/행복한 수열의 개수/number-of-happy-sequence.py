ccca1a56578c4b92f00461af1b56eb74d80a0fe0
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-07-16 17:01:30 +0900

    Title: 행복한 수열의 개수, Time: 162ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240716/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,53 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().rstrip().split())
+happy = 0
+grid = []
+for i in range(n):
+    grid.append(list(map(int, sys.stdin.readline().rstrip().split())))
+
+for r in range(n):
+    max_cnt = 1
+    cnt = 1
+    # print(f"<row {r}>")
+    for j in range(n):
+        if j+1 >= n:
+            continue
+
+        if grid[r][j] == grid[r][j+1]:
+            cnt += 1
+        elif grid[r][j] != grid[r][j+1]:
+            cnt = 1
+
+        if cnt >= max_cnt:
+                max_cnt = cnt
+
+    #     print(f"{grid[r][j]} - {grid[r][j+1]} : {cnt}, {max_cnt}")
+    # print(f"row {r}:{max_cnt}")
+    
+    if max_cnt >= m:
+        happy += 1
+
+for c in range(n):
+    max_cnt = 1
+    cnt = 1
+    # print(f"<col {c}>")
+    for i in range(n):
+        if i+1 >= n:
+            continue
+
+        if grid[i][c] == grid[i+1][c]:
+            cnt += 1
+        elif grid[i][c] != grid[i+1][c]:
+            cnt = 1
+
+        if cnt >= max_cnt:
+                max_cnt = cnt
+                
+    #     print(f"{grid[i][c]} - {grid[i+1][c]} : {cnt}, {max_cnt}")
+    # print(f"col {c}:{max_cnt}")
+    
+    if max_cnt >= m:
+        happy += 1
+
+print(happy)
