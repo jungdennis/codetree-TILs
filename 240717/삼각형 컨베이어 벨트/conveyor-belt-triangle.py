b689106c13ec5a073c504a001d244ffa9d10e908
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-07-17 15:53:25 +0900

    Title: 삼각형 컨베이어 벨트, Time: 130ms, Memory: 25MB, Status: Passed - CodeTree

--- 240717/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
+++ 240717/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
@@ -1,17 +1,47 @@
-import sys
+# 변수 선언 및 입력
+n, t = tuple(map(int, input().split()))
+l = list(map(int, input().split()))
+r = list(map(int, input().split()))
+d = list(map(int, input().split()))
 
-n, t = map(int, sys.stdin.readline().rstrip().split())
+for _ in range(t):
+    # Step 1
+    # 왼쪽에서 가장 오른쪽에 있는 숫자를 따로 temp값에 저장해놓습니다.
+    temp = l[n - 1]
+    
+    # Step 2
+    # 왼쪽에 있는 숫자들을 완성합니다. 
+    # 벨트를 기준으로 오른쪽에서부터 채워넣어야 하며, 
+    # 맨 왼쪽 숫자는 아래에서 가져와야함에 유의합니다.
+    for i in range(n - 1, 0, -1):
+        l[i] = l[i - 1]
+    l[0] = d[n - 1]
+    
+    # Step 3
+    # 오른쪽에 있는 숫자들을 완성합니다. 
+    # 벨트를 기준으로 마찬가지로 오른쪽에서부터 채워넣어야 하며, 
+    # 맨 왼쪽 숫자는 이전 단계에서 미리 저장해놨던 temp값을 가져와야함에 유의합니다.
+    temp2 = r[n - 1]
+    for i in range(n - 1, 0, -1):
+        r[i] = r[i - 1]
+    r[0] = temp
+    
+    # Step 4
+    # 아래에 있는 숫자들을 완성합니다. 
+    # 마찬가지로 벨트를 기준으로 오른쪽에서부터 채워넣어야 하며, 
+    # 맨 왼쪽 숫자는 이전 단계에서 미리 저장해놨던 temp값을 가져와야함에 유의합니다.
+    for i in range(n - 1, 0, -1):
+        d[i] = d[i - 1]
+    d[0] = temp2
+    
+# 출력
+for elem in l:
+    print(elem, end=" ")
+print()
 
-belt = []
-for i in range(3):
-    belt += list(map(int, sys.stdin.readline().rstrip().split()))
+for elem in r:
+    print(elem, end=" ")
+print()
 
-temp = 0
-for i in range(t):
-    temp = belt[-1]
-    belt = [temp] + belt[:-1]
-
-for i in range(3):
-    for j in range(n):
-        print(belt[n*i+j], end=' ')
-    print()
+for elem in d:
+    print(elem, end=" ")
