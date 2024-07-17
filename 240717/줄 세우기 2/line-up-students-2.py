7bd50fd07909f09399511ce8195f04520b741118
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-07-17 15:28:09 +0900

    Title: 줄 세우기 2, Time: 208ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240717/줄 세우기 2/line-up-students-2.py
@@ -0,0 +1,10 @@
+n = int(input())
+student =[]
+for i in range(n):
+    h, w = map(int, input().split())
+    student.append({'h':h, 'w':w, 'n':i+1})
+
+student.sort(key=lambda x : (x['h'], -x['w']))
+
+for s in student:
+    print(s['h'], s['w'], s['n'])
