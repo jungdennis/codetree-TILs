7e5cf3656bab7bd9d60e792b69e64a9cdd978bd8
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-07-18 13:33:22 +0900

    Title: Date to Date, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240718/Date to Date/date-to-date.py
@@ -0,0 +1,16 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+day = [31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30]
+
+start_day = 0
+for i in range(m1):
+    start_day += day[i]
+start_day += d1
+
+end_day = 0
+for i in range(m2):
+    end_day += day[i]
+end_day += d2
+
+
+print(end_day - start_day + 1)
