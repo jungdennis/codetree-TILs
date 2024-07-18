a24ec75ce9dd111f92d92ac73cf7aeffc3533e85
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-07-18 14:50:51 +0900

    Title: 최대로 겹치는 지점, Time: 104ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240718/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -0,0 +1,10 @@
+arr = [-1] + [0 for i in range(100)]
+n = int(input())
+
+for i in range(n):
+    x1, x2 = map(int, input().split())
+
+    for j in range(x1, x2+1):
+        arr[j] += 1
+
+print(max(arr))
