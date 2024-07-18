4e118e720953b451ed37782786fa33ee0ee6d545
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-07-18 13:39:22 +0900

    Title: Date to Date, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 240718/Date to Date/date-to-date.py
+++ 240718/Date to Date/date-to-date.py
@@ -1,19 +1,15 @@
 m1, d1, m2, d2 = map(int, input().split())
 
-day = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+day = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-if (m1 == m2) and (d1 == d2):
-    print(1)
-else:
-    start_day = 0
-    for i in range(m1):
-        start_day += day[i]
-    start_day += d1
+start_day = 0
+for i in range(1, m1):
+    start_day += day[i]
+start_day += d1
 
-    end_day = 0
-    for i in range(m2):
-        end_day += day[i]
-    end_day += d2
+end_day = 0
+for i in range(1, m2):
+    end_day += day[i]
+end_day += d2
 
-
-    print(end_day - start_day)
+print(end_day - start_day + 1)
