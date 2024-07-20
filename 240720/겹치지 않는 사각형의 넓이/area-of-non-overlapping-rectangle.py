ba9263a36dc5d81a127545fe5f656fff2f83185e
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-07-20 13:13:30 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 269ms, Memory: 55MB, Status: Passed - CodeTree

--- 240720/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
+++ 240720/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
@@ -1,6 +1,20 @@
 xa1, ya1, xa2, ya2 = map(int, input().split())
+xa1 += 1000
+xa2 += 1000
+ya1 += 1000
+ya2 += 1000
+
 xb1, yb1, xb2, yb2 = map(int, input().split())
+xb1 += 1000
+xb2 += 1000
+yb1 += 1000
+yb2 += 1000
+
 xm1, ym1, xm2, ym2 = map(int, input().split())
+xm1 += 1000
+xm2 += 1000
+ym1 += 1000
+ym2 += 1000
 
 arr = [[0 for i in range(-1000, 1001)] for j in range(-1000, 1001)]
 
