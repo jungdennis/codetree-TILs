f564948e757e2d74901eb8b2d57513db0d7963e0
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-07-29 18:17:44 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240729/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -0,0 +1,35 @@
+import sys
+
+n, r, c = map(int, sys.stdin.readline().rstrip().split())
+r -= 1
+c -= 1
+
+def is_range(x, y):
+    return x >= 0 and x < n and y >= 0 and y < n
+
+arr = []
+for i in range(n):
+    arr.append(list(map(int, sys.stdin.readline().rstrip().split())))
+
+visited = []
+
+drs = [0, 0, -1, 1]
+dcs = [1, -1, 0, 0]
+while True:
+    visited.append(arr[r][c])
+
+    flag = False
+    for dr, dc in zip(drs, dcs):
+        nr, nc = r + dr, c + dc
+
+        if is_range(nr, nc) and arr[nr][nc] > arr[r][c]:
+            r_, c_ = nr, nc
+            flag = True
+
+    if flag:
+        r, c = r_, c_
+    else:
+        break
+
+for num in visited:
+    print(num, end = ' ')
