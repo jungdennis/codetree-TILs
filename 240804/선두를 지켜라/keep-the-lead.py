39480a70b8361a77b8f8b9725c39bf7cd37b3dd1
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-08-04 18:09:30 +0900

    Title: 선두를 지켜라, Time: 175ms, Memory: 39MB, Status: Passed - CodeTree

--- 240804/선두를 지켜라/keep-the-lead.py
+++ 240804/선두를 지켜라/keep-the-lead.py
@@ -15,23 +15,18 @@ for i in range(m):
     for j in range(t):
         move_B.append(move_B[-1] + v)
 
-first = []
-def find_first(t):
-    if move_A[t] > move_B[t]:
-        return 'A'
-    elif move_A[t] < move_B[t]:
-        return 'B'
-    else:
-        return False
-
-for i in range(1, len(move_A)):
-    f_ = find_first(i)
-    if f_:
-        first.append(f_)
-
+first = 0
 cnt = 0
-for i in range(len(first)-1):
-    if first[i] != first[i+1]:
-        cnt += 1
+for i in range(1, len(move_A)):
+    if move_A[i] > move_B[i]:
+        if first == 'B':
+            cnt += 1
+
+        first = 'A'
+    elif move_A[i] < move_B[i]:
+        if first == 'A':
+            cnt += 1
+
+        first = 'B'
 
 print(cnt)
