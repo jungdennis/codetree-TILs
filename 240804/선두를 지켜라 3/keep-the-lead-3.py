4830fb4ad56d368dc35f2938bca6c34186c8c23b
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-08-04 19:08:21 +0900

    Title: 선두를 지켜라 3, Time: 184ms, Memory: 45MB, Status: Passed - CodeTree

---
+++ 240804/선두를 지켜라 3/keep-the-lead-3.py
@@ -0,0 +1,38 @@
+n, m = map(int, input().split())
+
+move_A = [0]
+move_B = [0]
+
+for i in range(n):
+    v, t = map(int, input().split())
+
+    for j in range(t):
+        move_A.append(move_A[-1] + v)
+
+for i in range(m):
+    v, t = map(int, input().split())
+
+    for j in range(t):
+        move_B.append(move_B[-1] + v)
+
+first = 'AB'
+cnt = 0
+
+for i in range(1, len(move_A)):
+    if move_A[i] < move_B[i]:
+        if first != 'B':
+            cnt += 1
+
+        first = 'B'
+    elif move_A[i] > move_B[i]:
+        if first != 'A':
+            cnt += 1
+
+        first = 'A'
+    else:
+        if first != 'AB':
+            cnt += 1
+
+        first = 'AB'
+
+print(cnt)
