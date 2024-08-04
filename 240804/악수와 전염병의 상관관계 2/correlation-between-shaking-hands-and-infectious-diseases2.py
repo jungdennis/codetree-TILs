c8f8c4e77c93bff51838f4aaee339283c3f56ce7
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-08-04 18:49:04 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240804/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240804/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -6,16 +6,18 @@ spread = [-1] + [0 for i in range(n)]
 virus[p] = 1
 
 shake_info = []
-
 for i in range(t):
     shake_info.append(list(map(int, input().split())))
-
 shake_info.sort(key=lambda x: x[0])
 
 for t, x, y in shake_info:
-    if virus[x] == 1 and spread[x] <= k:
+    if virus[x] == 1 and spread[x] < k:
         spread[x] += 1
         virus[y] = 1
+    
+    if virus[y] == 1 and spread[y] < k:
+        spread[y] += 1
+        virus[x] = 1
 
 for i in range(1, n+1):
     print(virus[i], end='')
