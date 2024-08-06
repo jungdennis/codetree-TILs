aff7f9fb187f88cd80171c6ffa4a05942ba75337
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-08-06 13:52:41 +0900

    Title: 체크판위에서 2, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

--- 240806/체크판위에서 2/on-the-checkboard-2.py
+++ 240806/체크판위에서 2/on-the-checkboard-2.py
@@ -14,7 +14,7 @@ for r in range(R):
 
             for r_ in range(r+1, R):
                 for c_ in range(c+1, C):
-                    if arr[r_][c_] != color[-1] and r_ < R-1 and c_ < C-1:
+                    if arr[r_][c_] != color[-1] and arr[r_][c_] != arr[R-1][C-1] and r_ < R-1 and c_ < C-1:
                         cnt += 1
                         # print((r, c), (r_, c_))
 
