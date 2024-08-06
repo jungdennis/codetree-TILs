7c33a9224945f2bcbea3014a977c4db58539086c
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-08-06 17:59:16 +0900

    Title: 겹치지 않게 선분 고르기, Time: 5072ms, Memory: 30MB, Status: Time Limit Exceed - CodeTree

--- 240806/겹치지 않게 선분 고르기/select-segments-without-overlap.py
+++ 240806/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -12,27 +12,25 @@ max_cnt = 0
 
 def detect(s):
     check = [0] * (max_num+1)
-    flag = True
 
     for x1, x2 in s:
         for i in range(x1, x2+1):
             check[i] += 1
 
             if check[i] > 1:
-                flag = False
+                return False
     
-    return flag, check
+    return True
 
 
 def pick_line(x):
     if x >= n:
         global max_cnt
         set_pick = set(pick)
-        flag, debug = detect(set_pick)
-        # print(pick)
-        # print(debug)
-        if flag:
+
+        if detect(set_pick):
             max_cnt = max(max_cnt, len(set_pick))
+            
         return 
 
     for i in range(n):
