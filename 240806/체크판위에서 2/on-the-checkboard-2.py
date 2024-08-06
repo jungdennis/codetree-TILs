c8c1f63fc23d7b9167354df5e22a01b35b565cf4
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-08-06 13:51:46 +0900

    Title: 체크판위에서 2, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240806/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,21 @@
+R, C = map(int, input().split())
+
+arr = []
+for i in range(R):
+    arr.append(input().split())
+
+cnt = 0
+for r in range(R):
+    for c in range(C):
+        color = [arr[0][0]]
+
+        if arr[r][c] != color[-1] and r > 0 and c > 0:
+            color.append(arr[r][c])
+
+            for r_ in range(r+1, R):
+                for c_ in range(c+1, C):
+                    if arr[r_][c_] != color[-1] and r_ < R-1 and c_ < C-1:
+                        cnt += 1
+                        # print((r, c), (r_, c_))
+
+print(cnt)
