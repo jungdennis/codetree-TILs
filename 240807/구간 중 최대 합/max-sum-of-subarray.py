a2da1b4d4bb2e4ac6192bfa1b1d2701ccb4c77b3
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-08-07 23:12:36 +0900

    Title: 구간 중 최대 합, Time: 246ms, Memory: 24MB, Status: Passed - CodeTree

--- 240807/구간 중 최대 합/max-sum-of-subarray.py
+++ 240807/구간 중 최대 합/max-sum-of-subarray.py
@@ -4,10 +4,13 @@ n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
 max_result = -sys.maxsize
-for i in range(n-k):
+for i in range(n-k+1):
     result = 0
+    # debug = []
     for j in range(k):
         result += arr[i+j]
+        # debug.append(arr[i+j])
+    # print(debug)
 
     max_result = max(max_result, result)
 
