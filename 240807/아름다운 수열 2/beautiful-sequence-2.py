bf2fb9874eb1f555eae980e4c0ec2182142a2293
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-08-07 23:46:16 +0900

    Title: 아름다운 수열 2, Time: 175ms, Memory: 24MB, Status: Passed - CodeTree

--- 240807/아름다운 수열 2/beautiful-sequence-2.py
+++ 240807/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,10 +1,12 @@
 n, m = map(int, input().split())
 A = list(map(int, input().split()))
-B = set(map(int, input().split()))
+B = list(map(int, input().split()))
+B.sort()
 
 cnt = 0
 for i in range(n-m+1):
-    A_part = set(A[i:i+m])
+    A_part = A[i:i+m]
+    A_part.sort()
 
     if A_part == B:
         cnt += 1
