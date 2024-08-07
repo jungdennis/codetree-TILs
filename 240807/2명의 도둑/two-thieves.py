22e591b533d28aabc9c8159adca65a020e24dc54
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-08-07 17:59:40 +0900

    Title: 2명의 도둑, Time: 113ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240807/2명의 도둑/two-thieves.py
+++ 240807/2명의 도둑/two-thieves.py
@@ -9,37 +9,36 @@ cost = []
 
 for i in range(N):
     for j in range(N-M+1):
-        for m in range(1, M+1):
-            weight = 0
-            c = 0
-            for k in range(m):
-                weight += arr[i][j+k]
-                c += arr[i][j+k] ** 2
+        weight = 0
+        c = 0
+        thing = []
+        for k in range(M):
+            thing.append(arr[i][j+k])
 
-            if weight <= C:
-                location.append((i, j, m))
-                cost.append(c)
-# print(location)
-# print(cost)
+        thing.sort(reverse=True)
+        for w in thing:
+            if weight + w > C:
+                pass
+            else:
+                weight += w
+                c += w ** 2
+
+        location.append((i, j))
+        cost.append(c)
+
+# for i in range(len(cost)):
+#     print(location[i], cost[i])
+
 max_cost = 0
 steal = []
 def overlap(info0, info1):
-    r0, c0, len0 = info0
-    r1, c1, len1 = info1
+    r0, c0 = info0
+    r1, c1 = info1
 
     if r0 != r1:
         return False
     else:
-        if c0 <= c1:
-            min_c = c0
-            min_len = len0
-            max_c = c1
-        else:
-            min_c = c1
-            min_len = len1
-            max_c = c0
-
-        if min_c + min_len <= max_c:
+        if min(c0, c1) + M <= max(c0, c1):
             return False
         else:
             return True
@@ -48,18 +47,19 @@ def two_thief(x):
     if x >= 2:
         global max_cost
 
-        r0, c0, len0, cost0 = steal[0]
-        r1, c1, len1, cost1 = steal[1]
+        r0, c0, cost0 = steal[0]
+        r1, c1, cost1 = steal[1]
 
-        if not overlap((r0, c0, len0), (r1, c1, len1)):
-            # print((r0, c0, len0), (r1, c1, len1), cost0, cost1)
-            max_cost = max(max_cost, cost0+cost1)
+        if not overlap((r0, c0), (r1, c1)):
+            if max_cost < cost0 + cost1:
+                # print((r0, c0), (r1, c1), cost0, cost1)
+                max_cost = max(max_cost, cost0+cost1)
 
         return
 
     for i in range(len(location)):
-        r, c, l = location[i]
-        steal.append((r, c, l, cost[i]))
+        r, c = location[i]
+        steal.append((r, c, cost[i]))
         two_thief(x+1)
         steal.pop()
 
