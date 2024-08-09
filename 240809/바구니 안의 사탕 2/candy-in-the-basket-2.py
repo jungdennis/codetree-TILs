536311f345d38ccacdf45e04e97dbec4b99cdf00
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-08-09 12:35:00 +0900

    Title: 바구니 안의 사탕 2, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240809/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240809/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -3,12 +3,13 @@ n, k = map(int, input().split())
 arr = [0] * 101
 for i in range(n):
     cnt, idx = map(int, input().split())
-    arr[idx] = cnt
+    arr[idx] += cnt
 
 import sys
 max_candy = -sys.maxsize
-for c in range(1, 100-2 * k+1):
+for c in range(101 - 2*k + 1):
     candy = sum(arr[c:c+2*k+1])
+    # print((c, c+2*k), arr[c:c+2*k+1], candy)
     max_candy = max(max_candy, candy)
 
 print(max_candy)
