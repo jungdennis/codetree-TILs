d75f4f3eee213eea1fe0aee7538a5a8dbade49ab
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-08-10 12:12:06 +0900

    Title: 단순한 동전 챙기기, Time: 109ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240810/단순한 동전 챙기기/collect-coins-easy.py
@@ -0,0 +1,52 @@
+import sys
+
+n = int(input())
+arr = []
+
+for i in range(n):
+    arr.append(list(input()))
+
+start = (0, 0)
+end = (0, 0)
+
+coin = []
+for r in range(n):
+    for c in range(n):
+        if arr[r][c] == 'S':
+            start = (r, c, 0)
+        elif arr[r][c] == 'E':
+            end = (r, c, 6)
+        elif arr[r][c] != '.':
+            coin.append((r, c, int(arr[r][c])))
+
+coin.sort(key=lambda x:x[2])
+
+picked = []
+min_distance = sys.maxsize
+def pick(n_pick, last_idx):
+    if n_pick >= 3:
+        global min_distance
+
+        route = [start] + picked + [end]
+        distance = 0
+
+        for i in range(4):
+            x1, y1, _ = route[i]
+            x2, y2, _ = route[i+1]
+            distance += (abs(x1 - x2) + abs(y1 - y2))
+
+        min_distance = min(distance, min_distance)
+
+    for i in range(len(coin)):
+        if i <= last_idx:
+            continue
+
+        picked.append(coin[i])
+        pick(n_pick + 1, i)
+        picked.pop()
+
+if len(coin) < 3:
+    print(-1)
+else:
+    pick(0, -1)
+    print(min_distance)
