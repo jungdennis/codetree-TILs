2c27392b89657c5df5bff4077a0c0a8b63307389
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-08-12 23:17:06 +0900

    Title: 그래프 탐색, Time: 5072ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 240812/그래프 탐색/graph-traversal.py
+++ 240812/그래프 탐색/graph-traversal.py
@@ -2,11 +2,11 @@ import sys
 
 n, m = map(int, input().split())
 
-arr = [[0 for i in range(n+1)] for i in range(n+1)]
+graph = [[] for i in range(n+1)]
 for i in range(m):
     start, end = map(int, input().split())
-    arr[start][end] = 1
-    arr[end][start] = 1
+    graph[start].append(end)
+    graph[end].append(start)
 
 visited = [1, 1] + [0] * (n-1)
 node = []
@@ -16,8 +16,8 @@ def move(last_node):
     if last_node != 1 and last_node not in approach:
         approach.append(last_node)
 
-    for i in range(1, n+1):
-        if arr[last_node][i] == 1 and visited[i] == 0:
+    for i in graph[last_node]:
+        if visited[i] == 0:
             node.append(i)
             visited[i] = 1
 
