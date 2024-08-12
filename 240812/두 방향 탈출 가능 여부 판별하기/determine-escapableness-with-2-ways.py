8f8bcab7f9ba6e8a5a6d48ba9356ff7129848470
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-08-12 23:39:44 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240812/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,35 @@
+n, m = map(int, input().split())
+
+arr = []
+for i in range(n):
+    arr.append(list(map(int, input().split())))
+visited =[[0 for i in range(m)] for j in range(n)]
+
+ganeung = 0
+
+def in_range(r, c):
+    return r >= 0 and r < n and c >= 0 and c < m
+
+def can_go(r, c):
+    if not in_range(r, c) or visited[r][c] == 1 or arr[r][c] == 1:
+        return False
+    else:
+        return True
+
+def move(r, c):
+    global ganeung
+
+    if r == n-1 and c == m-1:
+        ganeung = 1
+        sys.exit()
+
+    visited[r][c] = 1
+
+    for dr, dc in [(1, 0), (0, 1)]:
+        nr, nc = r + dr, c + dc
+
+        if can_go(nr, nc):
+            move(nr, nc)
+
+move(0, 0)
+print(ganeung)
