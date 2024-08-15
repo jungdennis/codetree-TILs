b5f002ccfd43c05e9cd48a8076637d4cc2cfb4bb
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-08-15 16:20:10 +0900

    Title: 겹치지 않는 선분 2, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240815/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240815/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -11,18 +11,24 @@ def check_overlap(line1, line2):
     x1_1, x1_2 = line1
     x2_1, x2_2 = line2
 
-    a1 = 1 / (x1_2 - x1_1)
-    a2 = 1 / (x2_2 - x2_1)
+    x1_min, x1_max = sorted(line1)
+    x2_min, x2_max = sorted(line2)
 
-    if a1 == a2:
+    if max(x1_min, x2_min) > min(x1_max, x2_max):
         return False
     else:
-        overlap_x = (a2 * x2_1 - a1 * x1_1) / (a2 - a1)
-        overlap_y = a1 * a2 * (x2_1 - x1_1) / (a2 - a1)
-        if overlap_x >= max(x1_1, x2_1) and overlap_x <= min(x1_2, x2_2) and overlap_y >= 0 and overlap_y <= 1:
-            return True
-        else:
+        a1 = 1 / (x1_2 - x1_1)
+        a2 = 1 / (x2_2 - x2_1)
+
+        if a1 == a2:
             return False
+        else:
+            overlap_x = (a2 * x2_1 - a1 * x1_1) / (a2 - a1)
+            overlap_y = a1 * a2 * (x2_1 - x1_1) / (a2 - a1)
+            if overlap_x >= max(x1_1, x2_1) and overlap_x <= min(x1_2, x2_2) and overlap_y >= 0 and overlap_y <= 1:
+                return True
+            else:
+                return False
 
 for i in range(n):
     for j in range(i+1, n):
