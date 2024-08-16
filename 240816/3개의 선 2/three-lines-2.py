7b6807582f2b594bfe24039a9a917793afaa6a46
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-08-16 16:57:35 +0900

    Title: 3개의 선 2, Time: 171ms, Memory: 25MB, Status: Passed - CodeTree

--- 240816/3개의 선 2/three-lines-2.py
+++ 240816/3개의 선 2/three-lines-2.py
@@ -11,6 +11,13 @@ for i in range(11):
         for k in range(11):
             cnt = 0
             for x, y in point:
+                if x == i or x == j or x == k:
+                    cnt += 1
+            if cnt == n:
+                flag = 1
+
+            cnt = 0
+            for x, y in point:
                 if x == i or x == j or y == k:
                     cnt += 1
             if cnt == n:
@@ -19,6 +26,13 @@ for i in range(11):
             cnt = 0
             for x, y in point:
                 if x == i or y == j or y == k:
+                    cnt += 1
+            if cnt == n:
+                flag = 1
+
+            cnt = 0
+            for x, y in point:
+                if y == i or y == j or y == k:
                     cnt += 1
             if cnt == n:
                 flag = 1
