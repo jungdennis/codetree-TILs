e72f50c270993a5c0131903dc40dc51a69ceb633
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-08-16 16:17:49 +0900

    Title: 야바위, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240816/야바위/ya-rock.py
+++ 240816/야바위/ya-rock.py
@@ -7,12 +7,13 @@ for i in range(n):
     a, b, c = map(int, input().split())
     case.append((a, b, c))
 
-stone = [0] * 4
 max_score = -sys.maxsize
 
 for i in range(1, 4):
+    stone = [0] * 4
     stone[i] = 1
     score = 0
+
     for a, b, c in case:
         stone[a], stone[b] = stone[b], stone[a]
         
