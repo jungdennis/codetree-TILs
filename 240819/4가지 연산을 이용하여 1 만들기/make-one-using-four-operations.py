15be09867537ff0322fcb3110d7e5429fe82d31d
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-08-19 22:24:51 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 178ms, Memory: 32MB, Status: Passed - CodeTree

--- 240819/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
+++ 240819/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -2,7 +2,9 @@ import sys
 from collections import deque
 
 n = int(input())
+max_n = 1000000
 q = deque()
+visited = [False for i in range(max_n + 1)]
 
 def calc(n, x):
     if x == 0:
@@ -14,25 +16,30 @@ def calc(n, x):
     elif x == 3:
         return n // 3
 
+def in_range(x):
+    return x >= 0 and x <= max_n
+
 def bfs():
     while q:
-        n, cnt = q.popleft()
+        x, cnt = q.popleft()
 
-        if n == 1:
+        if x == 1:
             return cnt
         else:
             calc_list = [0, 1]
-
-            if n % 2 == 0:
+            if x % 2 == 0:
                 calc_list.append(2)
-            if n % 3 == 0:
+            if x % 3 == 0:
                 calc_list.append(3)
 
             for c in calc_list:
-                new_n = calc(n, c)
-                q.append((new_n, cnt + 1))
-
+                nx = calc(x, c)
+                
+                if in_range(nx) and not visited[nx]:
+                    visited[nx] = True
+                    q.append((nx, cnt + 1))
 
 q.append((n, 0))
+visited[n] = True
 cnt = bfs()
 print(cnt)
