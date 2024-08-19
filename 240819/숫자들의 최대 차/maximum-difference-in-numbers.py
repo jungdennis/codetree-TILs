1a32bae3bede7146226159a5cd5cfb9010088287
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-08-19 18:08:47 +0900

    Title: 숫자들의 최대 차, Time: 150ms, Memory: 34MB, Status: Passed - CodeTree

--- 240819/숫자들의 최대 차/maximum-difference-in-numbers.py
+++ 240819/숫자들의 최대 차/maximum-difference-in-numbers.py
@@ -9,7 +9,7 @@ arr.sort()
 ans = []
 for i in range(n):
     for j in range(i+1, n):
-        if arr[j] - arr[i] > k:
-            ans.append(abs(i-j))
+        if arr[j] - arr[i] <= k:
+            ans.append(abs(j-i+1))
 
 print(max(ans))
