6c87b639609f9637af56d96438a2076a2e2ba82d
jungdennis <68803616+jungdennis@users.noreply.github.com>
2024-08-19 18:37:08 +0900

    Title: 훌륭한 점프, Time: 172ms, Memory: 25MB, Status: Passed - CodeTree

--- 240819/훌륭한 점프/great-jump.py
+++ 240819/훌륭한 점프/great-jump.py
@@ -16,10 +16,13 @@ for i in range(n):
     route.append(n-1)
 
     flag = True
-    for i in range(len(route)-1):
-        if abs(route[i+1] - route[i]) > k:
-            flag = False
-            break
+    if arr[0] > th or arr[-1] > th:
+        flag = False
+    else:
+        for j in range(len(route)-1):
+            if abs(route[j+1] - route[j]) > k:
+                flag = False
+                break
 
     if flag:
         ans = min(ans, th)
